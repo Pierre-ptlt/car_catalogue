6e7fd06b297260eb74e3957f0d0ad4cfe1a41b56
Pierre-ptlt <petillot.pierre@hotmail.fr>
2023-09-10 17:36:07 +0200

    architecture principale mise en place

---
+++ components/Hero.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Image from 'next/image';
+
+const Hero = () => {
+  return (
+    <div className="text-green-500">
+      hero
+    </div>
+  )
+}
+
+export default Hero
