a51581c13addcaadc266afcac860f235081ef884
Pierre-ptlt <petillot.pierre@hotmail.fr>
2023-09-10 16:47:32 +0200

    app initialisée

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="overflow-hidden">
+      <p className="text-green-600">Homepage</p>
+    </main>
+  )
+}
